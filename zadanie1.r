3811fec03c10274692c1b9aa5dbd15495d371790
kacpermuszynski <kacper.muszynski@yahoo.com>
2024-01-27 21:24:33 +0100

    Fixed some of the problem in task 2

--- zadanie1.r
+++ zadanie1.r
@@ -8,18 +8,18 @@ print(test_result)
 # Liczba Wypadków zgrupowanych przez miesiące w roku 2022
 accidents_per_month <- c(1300, 1220, 1584, 1479, 2119, 2349, 2153, 2199, 1941, 2009, 1506, 1463)
 days_in_months <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-accidents_by_days <- accidents_per_month / days_in_months
+accidents_by_months_reg_days <- accidents_per_month / days_in_months
 
-test_result <- chisq.test(accidents_by_days)
+test_result <- chisq.test(accidents_by_months_reg_days)
 
 print(test_result)
 
 # Liczba zgonów zgrupowanych przez miesiące w roku 2022
 deaths_per_month <- c(137, 96, 114, 111, 147, 156, 169, 173, 157, 151, 143, 115)
 days_in_months <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-deaths_by_days <- deaths_per_month / days_in_months
+deaths_by_months_reg_days <- deaths_per_month / days_in_months
 
-test_result <- chisq.test(deaths_by_days)
+test_result <- chisq.test(deaths_by_months_reg_days)
 
 print(test_result)
 
--- zadanie2.r
+++ zadanie2.r
@@ -1,16 +1,16 @@
-# Załaduj potrzebne pakiety
-library(nortest) # dla testu Andersona-Darlinga
 
-# Ustal parametry symulacji
+library(nortest)
+
+
 n <- 20 # rozmiar próby
 alpha <- 0.05 # poziom istotności
 simulations <- 10000 # liczba symulacji
 
-# Inicjalizuj liczniki
+
 shapiro_rejections <- 0
 anderson_rejections <- 0
 
-# Przeprowadź symulacje dla rozkładu normalnego
+
 for (i in 1:simulations) {
   sample <- rnorm(n)
   if (shapiro.test(sample)$p.value < alpha) {
@@ -21,12 +21,12 @@ for (i in 1:simulations) {
   }
 }
 
-# Wydrukuj wyniki
+
 cat("Shapiro-Wilk test rejected H0", shapiro_rejections, "times out of", simulations, "\n")
 cat("Anderson-Darling test rejected H0", anderson_rejections, "times out of", simulations, "\n")
 
-# Przeprowadź symulacje dla rozkładu t-Studenta
-df_values <- c(5, 10, 30) # wybrane wartości stopni swobody
+
+df_values <- c(5, 10, 30)
 
 for (df in df_values) {
   shapiro_rejections <- 0
@@ -40,7 +40,7 @@ for (df in df_values) {
       anderson_rejections <- anderson_rejections + 1
     }
   }
-  # Wydrukuj wyniki
+
   cat("For df =", df, ":\n")
   cat("Shapiro-Wilk test correctly rejected H0", shapiro_rejections, "times out of", simulations, "\n")
   cat("Anderson-Darling test correctly rejected H0", anderson_rejections, "times out of", simulations, "\n")
